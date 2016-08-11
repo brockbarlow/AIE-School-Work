a39c1e4b556d4df6915626d4b04d145fffc857e8
brockbarlow <brock.barlow@students.aie.edu.au>
2016-08-11 14:14:08 -0500

    Made edits to the header.h file

--- 8-11-2016/Vector Refresher/Vector Refresher/Header.h
+++ 8-11-2016/Vector Refresher/Vector Refresher/Header.h
@@ -1,1 +1,48 @@
-#pragma once
+#include <iostream>
+
+template<typename T>
+class vector {
+public:
+	T x, y, z;
+
+	void print()
+	{
+		std::cout << x << " " << y << " " << z << std::endl;
+	}
+
+	vector()
+	{
+		x, y, z = 0;
+	}
+
+	vector(T a, T b)
+	{
+		x = a;
+		y = b;
+	}
+
+	vector(T a, T b, T c)
+	{
+		x = a;
+		y = b;
+		z = c;
+	}
+
+	vector operator+(vector other)
+	{
+		vector temp(0, 0);
+		temp.x = x + other.x;
+		temp.y = y + other.y;
+		temp.z = z + other.z;
+		return temp;
+	}
+
+	vector operator-(vector other)
+	{
+		vector temp(0, 0);
+		temp.x = x - other.x;
+		temp.y = x - other.y;
+		temp.z = z - other.z;
+		return temp;
+	}
+};
