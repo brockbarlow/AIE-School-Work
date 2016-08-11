487c083357073450119d332bb97d3e4dc8aca30a
brockbarlow <brock.barlow@students.aie.edu.au>
2016-08-11 11:34:39 -0500

    Added to the example. Testing Team Explorer commit.

--- 8-11-2016/Double Inheritance/Double Inheritance/Source.cpp
+++ 8-11-2016/Double Inheritance/Double Inheritance/Source.cpp
@@ -12,6 +12,10 @@ public:
 	virtual void yell() {
 		std::cout << "bark" << std::endl;
 	}
+
+	virtual void jump() {
+		std::cout << "dog jump" << std::endl;
+	}
 };
 
 class Cat : public animal
@@ -20,30 +24,42 @@ public:
 	virtual void yell() {
 		std::cout << "meow" << std::endl;
 	}
+
+	virtual void jump() {
+		std::cout << "cat jump" << std::endl;
+	}
 };
 
 class Beagal : public Dog
 {
 public:
 	void walk() override {
-		std::cout << "walking here..." << std::endl;
+		std::cout << "dog walking here..." << std::endl;
 	}
 
 	/*void yell() override {
 		std::cout << "beagal" << std::endl;
 	}*/
+
+	void jump() override {
+		std::cout << "beagal jumper" << std::endl;
+	}
 };
 
 class Persian : public Cat
 {
 public:
 	void walk() override {
-		std::cout << "walking here..." << std::endl;
+		std::cout << "cat walking here..." << std::endl;
 	}
 
 	/*void yell() override {
 		std::cout << "persian" << std::endl;
 	}*/
+
+	void jump() override {
+		std::cout << "persian jumper" << std::endl;
+	}
 };
 
 int main()
@@ -53,9 +69,11 @@ int main()
 
 	b.walk();
 	b.yell();
+	b.jump();
 
 	p.walk();
 	p.yell();
+	p.jump();
 
 	system("pause");
 	return 0;
