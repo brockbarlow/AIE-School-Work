556b79d5753a8fc4c9c8e2ceb0871d519a7d22e8
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-06-07 12:43:21 -0500

    Added Project Commit
    added gist 2 redo project to repo

---
+++ Gist 2 Redo/Gist 2 Redo/Header.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+
+class Identity
+{
+public:
+	int rows;
+	int cols;
+
+	Identity()
+	{
+		rows = 0;
+		cols = 0;
+	}
+
+	void createIdentity(int r, int c)
+	{
+
+	}
+};
+
+//void createIdentity(int r, int c)
+//{
+//
+//}
---
+++ Gist 2 Redo/Gist 2 Redo/Source.cpp
@@ -0,0 +1,26 @@
+/*Gist 2 Description:
+"Write a c++ program that creates the identity matrix. When they are done, run it from
+the desktop and output it."*/
+
+#include "Header.h"
+
+//void createIdentity(int, int);
+
+//int rows;
+//int cols;
+
+int main()
+{
+	//std::cout << "Enter # of rows: " << std::endl;
+	//std::cin >> rows;
+	//std::cout << std::endl;
+
+	//std::cout << "Enter # of columns: " << std::endl;
+	//std::cin >> cols;
+	//std::cout << std::endl;
+
+	//createIdentity(rows, cols);
+
+	system("pause");
+	return 0;
+}
